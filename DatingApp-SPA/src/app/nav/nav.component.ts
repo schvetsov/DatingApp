db0203989f28ab959514de48bd82e3fdc6db80af
Stan Chvetsov <stan@Stans-MacBook-Pro.local>
2018-12-10 16:51:30 -0500

    added auth0 library

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -24,8 +24,9 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
+    // const token = localStorage.getItem('token');
+    // return !!token;
   }
 
   logout() {
